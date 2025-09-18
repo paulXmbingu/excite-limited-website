fb8eb93bd3735569f5d081144b0f152cc7865710
Paul Mbingu <paulxmbingu@gmail.com>
2025-09-19 01:33:12 +0300

    Update

--- src/app/home-screen.tsx
+++ src/app/home-screen.tsx
@@ -1,64 +1,90 @@
 "use client";
 
-import { BookOpen01, Cube01, HelpCircle } from "@untitledui/icons";
-import { Button } from "@/components/base/buttons/button";
-import {ExciteLogo} from "@/components/foundations/logo/excite-logo"
+import React from "react";
 import { useClipboard } from "@/hooks/use-clipboard";
+import { ExciteLogo } from "@/components/foundations/logo/excite-logo";
+import { SocialButton } from "@/components/base/buttons/social-button";
+import styles from "./home-screen.module.css";
 
 export const HomeScreen = () => {
-    const clipboard = useClipboard();
+  const clipboard = useClipboard()
+  return (
+    <div className={styles.homePageWrap}>
+      {/* Media Section */}
+      <div className={styles.mediaSection}>
+        <video
+          autoPlay
+          playsInline
+          preload="auto"
+          muted
+          loop
+          height="100%"
+          className={styles.media}
+        >
+          <source
+            src="https://res.cloudinary.com/excit3/video/upload/v1725643817/Excite%20Company%20Website/Next-Gen_Fintech_and_Banking_Products_Showreel_2023_by_Financial_UX_Design_Agency_UXDA_hrm7d3.mp4"
+            type="video/mp4"
+          />
+        </video>
+        <div className={styles.mediaSectionScrim}></div>
+      </div>
 
-    return (
-        <div className="flex h-dvh flex-col">
-            <div className="flex min-h-0 flex-1 flex-col items-center justify-center px-4 md:px-8">
-                
-                <div className="relative flex w-64 items-center justify-center m-10">
-                    <ExciteLogo />
-                </div>
+      {/* Details Section */}
+      <div className={styles.detailsSection}>
+        <div className={styles.pageDetails}>
+          <div className="relative flex w-128 items-center justify-center m-10">
+            <ExciteLogo />
+          </div>
 
-                <h1 className="max-w-3xl text-center text-display-xsm font-regular text-primary"> Premier Digital Product Innovation Company that Specializes in </h1>
-                
-                <p className="mt-1 max-w-xl text-center text-3xl font-bold leading-snug tracking-tight text-primary font-inter">
-                    UIUX Research, Design & Software Engineering
-                </p>
-
-                <div className="mt-6 flex items-center gap-3">
-                    <Button
-                        href="https://www.untitledui.com/react/docs/introduction"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        color="link-color"
-                        size="lg"
-                        iconLeading={BookOpen01}
-                    >
-                        Docs
-                    </Button>
-                    <div className="h-px w-4 bg-brand-solid" />
-                    
-                    <Button
-                        href="https://www.untitledui.com/react/resources/icons"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        color="link-color"
-                        size="lg"
-                        iconLeading={Cube01}
-                    >
-                        Icons
-                    </Button>
-                    <div className="h-px w-4 bg-brand-solid" />
-                    
-                    <Button
-                        href="https://github.com/untitleduico/react/issues"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        color="link-color"
-                        size="lg"
-                        iconLeading={HelpCircle}
-                    >
-                        Help
-                    </Button>
-                </div>
+          <div className={styles.textCTA}>
+            <div className={styles.subTextHeroText}>
+              <div className={styles.subText}>
+                Premier Digital Product Innovation Company Specializing in
+              </div>
+              <div className={styles.heroText}>
+                UIUX Research, Design & Software Engineering
+              </div>
             </div>
+
+            <div className={styles.cTAs}>
+              <div className={styles.socialMediaCTA}>
+                Follow us on our socials
+              </div>
+
+              <div className={styles.socialMediaIcons}>
+                <SocialButton
+                    social="dribble"
+                    theme="gray"
+                    href="https://www.linkedin.com/company/excitecompany"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                />                
+                <SocialButton
+                    social="apple"
+                    theme="gray"
+                    href="https://www.linkedin.com/company/excitecompany"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                />                
+                <SocialButton
+                    social="facebook"
+                    theme="gray"
+                    href="https://www.linkedin.com/company/excitecompany"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                />                
+                <SocialButton
+                    social="twitter"
+                    theme="gray"
+                    href="https://www.linkedin.com/company/excitecompany"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                />
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
